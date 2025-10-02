eb2addedf3e31c1858641aab6c57dfa16d4a5d49
Paillat-dev <paillat@pycord.dev>
2025-10-02 17:20:43 +0200

    :sparkles: Use @widgetbot/message-renderer instead of @skyra/discord-components-core

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,9 @@ import starlight from "@astrojs/starlight";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 
+import svelte from "@astrojs/svelte";
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
     site: "https://guide.pycord.dev",
@@ -126,6 +129,8 @@ export default defineConfig({
                 },
             ],
         }),
+        svelte(),
+        react(),
     ],
     vite: {
         plugins: [tailwindcss()],
